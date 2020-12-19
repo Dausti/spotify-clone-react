e416d715b1c3a42dcb5996be4ca8f5c0ca0ccec8
Daniel Austi <danielausti@gmail.com>
2020-12-18 16:44:24 -0800

    updates

---
+++ src/Sidebar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
+import { getTokenFromResponse } from "./spotify";
+import { useStateValue } from "./StateProvider";
+
+function Sidebar() {
+  const [{ playlists }, dispatch] = useStateValue();
+  console.log(playlists);
+
+  return (
+    <div className="sidebar">
+      <img
+        className="sidebar__logo"
+        src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png"
+        alt=""
+      />
+      <SidebarOption Icon={HomeIcon} option="Home" />
+      <SidebarOption Icon={SearchIcon} option="Search" />
+      <SidebarOption Icon={LibraryMusicIcon} option="Your Library" />
+      <br />
+      <strong className="sidebar__title">PLAYLISTS</strong>
+      <hr />
+      {playlists?.items?.map((playlist) => (
+        <SidebarOption option={playlist.name} />
+      ))}
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/spotify.js
@@ -0,0 +1,27 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+const clientId = "19b79d34ab274296b0b3f8bd15a42587";
+const redirectUri = "https://spotify-clone-d1a29.web.app";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const getTokenFromResponse = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
